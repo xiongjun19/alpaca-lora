6194096de6787ba8245551c32ef0450f58a18e0f
Your Name <you@example.com>
2023-08-18 17:53:37 +0800

    fixing bug in finetue_prof

--- finetune_prof.py
+++ finetune_prof.py
@@ -264,7 +264,7 @@ def train(
             warmup_steps=100,
             num_train_epochs=num_epochs,
             learning_rate=learning_rate,
-            max_steps=5
+            max_steps=5,
             fp16=True,
             logging_steps=10,
             optim="adamw_torch",
