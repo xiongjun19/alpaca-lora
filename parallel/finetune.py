8eb85ded863b7de92038e2a9d2901022666e7431
JunXiong <jun.xiong@lightelligence.ai>
2023-08-28 18:00:09 +0800

    finished the dataset reading of alpaca_data

--- parallel/finetune.py
+++ parallel/finetune.py
@@ -114,7 +114,7 @@ def train_valid_test_datasets_provider(train_val_test_num_samples):
 
 
 def build_alpaca_ds():
-    data_path = "alpaca-cleaned.jsonl"
+    data_path = "alpaca_data_cleaned.json"
     data = load_dataset("json", data_files=data_path)
     val_set_size = 2000
     cutoff_len = 256
@@ -141,26 +141,28 @@ def generate_and_tokenize_prompt(data_point):
         data_point["output"],
     )
     tokenized_full_prompt = tokenize(full_prompt)
-    if not train_on_inputs:
-        user_prompt = prompter.generate_prompt(
-            data_point["instruction"], data_point["input"]
-        )
-        tokenized_user_prompt = tokenize(
-            user_prompt, add_eos_token=add_eos_token
-        )
-        user_prompt_len = len(tokenized_user_prompt["input_ids"])
-
-        if add_eos_token:
-            user_prompt_len -= 1
-
-        tokenized_full_prompt["labels"] = [
-            -100
-        ] * user_prompt_len + tokenized_full_prompt["labels"][
-            user_prompt_len:
-        ]  # could be sped up, probably
     return tokenized_full_prompt
 
 
+def tokenize(prompt, add_eos_token=True):
+    result = tokenizer(
+        prompt,
+        truncation=True,
+        max_length=cutoff_len,
+        padding=False,
+        return_tensors=None,
+    )
+    if (
+        result["input_ids"][-1] != tokenizer.eos_token_id
+        and len(result["input_ids"]) < cutoff_len
+        and add_eos_token
+    ):
+        result["input_ids"].append(tokenizer.eos_token_id)
+        result["attention_mask"].append(1)
+
+    result["labels"] = result["input_ids"].copy()
+    result["text"] = result["input_ids"].copy()
+    return result
 
 
 if __name__ == "__main__":
